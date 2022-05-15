60fc55f22b9decf5d5df9b4815b7706e882c65f1
guonan <838093891@qq.com>
2022-05-15 22:55:59 +0800

    add: 插件编写&打包

--- plugins/banner-webpack-plugin.js
+++ plugins/banner-webpack-plugin.js
@@ -3,11 +3,23 @@ class BannerWebpackPlugin {
     this.options = options;
   }
   apply(compiler) {
-    compiler.hooks.compilation.tap('BannerWebpackPlugin', compilation => {
-      compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('BannerWebpackPlugin', (data, cb) => {
-        data.html = data.html.replace('</head>', `<script>${this.options.content}</script></head>`);
-        cb(null, data);
-      });
+    compiler.hooks.compilation.tap('BannerWebpackPlugin', (compilation) => {
+      console.log('---compiler---')
+    })
+
+    compiler.hooks.emit.tapAsync('BannerWebpackPlugin', (compilation, cb) => {
+      debugger
+      compilation.assets['banner.text'] = {
+        source: () => {
+          return this.options.content
+        },
+        size: () => {
+          return this.options.content.length
+        }
+      }
+      cb()
     });
   }
 }
+
+module.exports = BannerWebpackPlugin;
